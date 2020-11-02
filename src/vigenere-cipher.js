1ffd85b461a4199a3c16c952e4a9381b53fb2ad4
General <general-m7@yandex.ru>
2020-11-02 23:22:06 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+    this.letter = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  }
+
+  encrypt(msg, key) {
+    const message = [...msg.toUpperCase()];
+    const keys = [...key.toUpperCase()];
+    let pos = 0;
+    let result = message.map(elem => {
+      if (this.letter.includes(elem)) {
+        let idx = (this.letter.indexOf(keys[pos]) + this.letter.indexOf(elem)) % this.letter.length;
+        pos = ++pos % keys.length;
+        return this.letter[idx];
+      }
+      return elem;
+
+    });
+    return this.type ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(msg, key) {
+    const message = [...msg.toUpperCase()];
+    const keys = [...key.toUpperCase()];
+    let pos = 0;
+    let result = message.map(elem => {
+      if (this.letter.includes(elem)) {
+        let idx = this.letter.indexOf(elem) - this.letter.indexOf(keys[pos]);
+        if (idx < 0) idx += this.letter.length;
+        pos = ++pos % keys.length;
+        return this.letter[idx];
+      }
+      return elem;
+
+    });
+    return this.type ? result.join('') : result.reverse().join('');
   }
 }
 
